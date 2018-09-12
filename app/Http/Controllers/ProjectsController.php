43f89fac8a757bd83c0f803294e6f8799be6c51b
haisham <haisham42@hotmail.com>
2018-09-12 23:30:05 +0200

    more-updates

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -69,7 +69,9 @@ class ProjectsController extends Controller
         $Entry->project_id = $projectId;
         $Entry->start_time = $startTime;
         $Entry->stop_time = $stopTime;
-        $Entry->diff = date_diff( date_create($startTime), date_create($stopTime));
+        $difference = date_diff( date_create($startTime), date_create($stopTime));
+        $formattedDifference = $diff->format('%h:%i');
+        $Entry->diff = $formattedDifference
         if($Entry->save()){
             $success = ['success'=>'true'];
             return response()->json($success);
